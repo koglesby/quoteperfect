6fac505e88f3d48f7b2aebeb194c79c1dd41576e
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-06-02 14:16:22 -0700

    added quotes list page

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
 
-/**
- * Generated class for the QuotesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit {
+  
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
+  
+  constructor(private navParams: NavParams){}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // ionViewDidLoad() {
+  //   this.quoteGroup = this.navParams.data;
+  // Add elvis operator (?) in template to use
+  // }
+
+
+  ngOnInit() {
+    this.quoteGroup = this.navParams.data;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
-  }
 
 }
